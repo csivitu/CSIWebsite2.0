ab5f795017ba7c712e1730463751cf4d2ec93f9a
v1br <vibhorag2003@gmail.com>
2025-02-21 22:26:21 +0530

    fix: gsap structure

--- js/heroAnimation.js
+++ js/heroAnimation.js
@@ -1,43 +1,74 @@
-const words = ["haos", "razy", "heerful", "omputer"];
-let index = 0;
-let charIndex = 0;
-let isDeleting = false;
-const typingElement = document.getElementById("typing");
+// Text animation
+(function typeEffect() {
+    
+    const words = ["haos", "razy", "heerful", "omputer"];
+    let index = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    const typingElement = document.getElementById("typing");
 
-function typeEffect() {
-    const currentWord = words[index];
-    if (!isDeleting) {
-        typingElement.textContent = currentWord.substring(0, charIndex);
-        if (charIndex <= words[index].length-1) typingElement.textContent += '_';
-        charIndex++;
-        if (charIndex > currentWord.length) {
-            isDeleting = true;
-            setTimeout(typeEffect, 1200);
+    (function repeatEffect() {
+        const currentWord = words[index];
+        if (!isDeleting) {
+            typingElement.textContent = currentWord.substring(0, charIndex);
+            if (charIndex <= words[index].length-1) typingElement.textContent += '_';
+            charIndex++;
+            if (charIndex > currentWord.length) {
+                isDeleting = true;
+                setTimeout(repeatEffect, 1200);
+                return;
+            }
+        } else {
+            typingElement.textContent = currentWord.substring(0, charIndex);
+            if (charIndex <= words[index].length-1) typingElement.textContent += '_';
+            charIndex--;
+            if (charIndex === 0) {
+                isDeleting = false;
+                index = (index + 1) % words.length;
+            }
+        }
+        
+        if (index == words.length-1 && isDeleting) {
             return;
         }
-    } else {
-        typingElement.textContent = currentWord.substring(0, charIndex);
-        if (charIndex <= words[index].length-1) typingElement.textContent += '_';
-        charIndex--;
-        if (charIndex === 0) {
-            isDeleting = false;
-            index = (index + 1) % words.length;
-        }
-    }
+        setTimeout(repeatEffect, isDeleting ? 50 : 100);
+    })();
+})();
+
+
+// Floating animation
+(function floatElements() {
+    gsap.to(".cross-1", {
     
-    if (index == words.length-1 && isDeleting) {
-        return;
-    }
-    setTimeout(typeEffect, isDeleting ? 50 : 100);
-}
+    });
+    
+    gsap.to(".circle-1", {
+    
+    });
+    
+    gsap.to(".line-1", {
+    
+    });
+    
+    gsap.to(".triangle-1", {
+    
+    });
 
-gsap.to(".floating-image", {
-    x: "random(-30, 30)", 
-    y: "random(-20, 20)", 
-    duration: 3, 
-    repeat: -1, 
-    yoyo: true, 
-    ease: "sine.inOut"
-});
+})();
 
-typeEffect();
+
+// Drag and drop animation
+(function () {
+    const waitForGSAP = setInterval(() => {
+        if (window.gsap && window.Draggable) {
+            clearInterval(waitForGSAP);
+            Draggable.create(".draggable", {
+
+                
+
+
+
+            });
+        }
+    }, 100);
+})();
